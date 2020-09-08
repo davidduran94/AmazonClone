9ad06207ef574dacd0b3b3fdc5339af4b83ddae5
David Duran <davidduran94@gmail.com>
2020-09-08 15:32:44 -0500

    Router

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Checkout from './components/Checkout'
 
 function App() {
   return (
     //BEM convection 
-    <div className="app">
-      
-
-      {/** HEADER */}
+    <Router>
+      <div className="app">
       <Header/>
+        <Switch>
+          <Route path="/home">
+            <Home/>
+          </Route>
 
-      {/** HOME */}
-      <Home/>
+          <Route path="/checkout">
+            <Checkout></Checkout>
+          </Route>
+          
+        </Switch>
+       
 
-    </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Checkout() {
+    return (
+        <div>
+           this is tejh chekcout 
+        </div>
+    )
+}
+
+export default Checkout
