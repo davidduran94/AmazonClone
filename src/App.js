8e00756d84f8db304d023a80fd6413868502df2b
David Duran <davidduran94@gmail.com>
2020-09-09 15:04:57 -0500

    Login front

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="app">
-        <Header />
-
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
+
+          
         </Switch>
       </div>
     </Router>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Login.css';
+import {Link} from 'react-router-dom';
+//import { useState } from '';
+
+
+function Login() {
+    //const [email, setEmail] = useState('');
+    //const [password, setPassword] = useState('');
+     
+    const signIn = (e) => {
+        e.preventDefault();
+
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div className="login">
+            <Link to="/">
+                <img
+                    className="login__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                />
+            </Link>
+            <div className="login__container">
+                <h1>Sing In</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input type="text" />
+
+                    <h5>Password</h5>
+                    <input type="password" />
+
+
+                </form>
+                <button type="submit" onClick={signIn} className="login__signinButton">Sign in</button>
+                <p>Amazon fake clone yore agree to sale ple see our privaicty Notica Cookies ans our interes Basen no add Notica</p>
+
+                <button className="login__registerButton">Create your amaozn account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
